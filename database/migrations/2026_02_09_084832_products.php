0bbafc993e0cfa41ab50b1564b11c9ee79a090c3
rdesfonds-campus <romain.desfonds@gmail.com>
2026-02-11 16:20:04 +0100

    11-02-26a

--- database/migrations/2026_02_09_084832_products.php
+++ database/migrations/2026_02_09_084832_products.php
@@ -6,25 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
-{
-    Schema::create('products', function (Blueprint $table) {
-        $table->id(); // Identifiant unique
-        $table->string('name'); // Nom du produit
-        $table->string('slug')->unique(); // Slug du produit
-        $table->text('description')->nullable(); // Description (optionnelle)
-        $table->decimal('price', 8, 2); // Prix (ex: 99.99)
-        $table->foreignId('category_id')->constrained()->onDelete('cascade'); // Clé étrangère vers la table "categories"
-        $table->timestamps(); // Créé le "created_at" et "updated_at"
-    });
-}
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
 
-    /**
-     * Reverse the migrations.
-     */
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+
+            $table->boolean('active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
     public function down(): void
     {
         Schema::dropIfExists('products');
